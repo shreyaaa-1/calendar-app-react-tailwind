389c2d10f33691a09b9890a1fa9a48ceeb35b986
Shreya Singh <shreyasingh47604@gmail.com>
2025-11-07 18:43:08 +0530

    chore: configure tailwind and base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: { extend: {} },
+  plugins: []
 }
-
